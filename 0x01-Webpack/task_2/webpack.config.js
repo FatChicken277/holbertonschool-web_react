c41c8d13f851f993c02d5f5cd896601dfd85635f
FatChicken277 <aramirez@truora.com>
2021-12-20 23:12:36 -0500

    add task_2

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpeg|jpg|svg)$/,
+        use: 'url-loader',
+        enforce: 'pre',
+      },
+    ],
+  },
+};
